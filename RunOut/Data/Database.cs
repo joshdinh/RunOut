4c5db6ce000804d3f00ce4c0a67c15d916ad80b9
joshdinh <thuy.dinh@ucalgary.ca>
2022-12-07 23:00:57 -0700

    Feature/login+signup
    Added ability to signup and login with authenticated user information

---
+++ RunOut/Data/Database.cs
@@ -0,0 +1,13 @@
+﻿namespace RunOut.Data
+{
+    public static class Database
+    {
+        //Create a dictionary to store usernames and passwords
+        public static Dictionary<string, string> users = new Dictionary<string, string>();
+        
+
+        //Dictionary to connect the user to their data 
+        public static Dictionary<string, UserData> userData = new Dictionary<string, UserData>();
+    }
+    
+}
