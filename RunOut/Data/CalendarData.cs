c54c5bbe4327ed9badf21b86cd2c6b52fe3028ed
yuminasd <43660270+yuminasd@users.noreply.github.com>
2022-12-11 16:53:48 -0700

    All user flows are working

--- RunOut/Data/CalendarData.cs
+++ RunOut/Data/CalendarData.cs
@@ -1,6 +1,7 @@
 ﻿using RunOut.Pages;
 using static RunOut.Pages.Calendar;
 using System.Globalization;
+using static RunOut.Data.UserData;
 
 namespace RunOut.Data
 {
@@ -12,39 +13,50 @@ namespace RunOut.Data
         public int currentDay = 1;
         public int currentMonth = 1;
 
-        public int raceDay = -1;
-        public int raceMonth = -1;
-
         public Interval nullInterval = new Interval(0, 0, 0, 0);
 
 		public Calendar_Data()
         {
-            //Week 1
-            runs.Add(new Day("Dec", "Thu", 8, "Rest", nullInterval, 0, "0", 2));
-            runs.Add(new Day("Dec", "Fri", 9, "Run", nullInterval, 1.5, "5:30", 2));
-            runs.Add(new Day("Dec", "Sat", 10, "Rest", nullInterval, 0, "0", 2));
-            runs.Add(new Day("Dec", "Sun", 11, "Interval", new Interval(2,2.5,3,3.5), 2.0, "5:30", 1));
-            runs.Add(new Day("Dec", "Sun", 11, "Interval", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
-            runs.Add(new Day("Dec", "Sun", 11, "Interval", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
-            runs.Add(new Day("Dec", "Sun", 11, "Interval", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
 
-            //Week 2
-            runs.Add(new Day("Dec", "Thu", 8, "Rest1", nullInterval, 0, "0", 0));
-            runs.Add(new Day("Dec", "Fri", 9, "Run2", nullInterval, 1.5, "5:30", 0));
-            runs.Add(new Day("Dec", "Sat", 10, "Rest3", nullInterval, 0, "0", 0));
-            runs.Add(new Day("Dec", "Sun", 11, "Interval4", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
-            runs.Add(new Day("Dec", "Sun", 11, "Interval5", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
-            runs.Add(new Day("Dec", "Sun", 11, "Interval6", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
-            runs.Add(new Day("Dec", "Sun", 11, "Interval7", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
 
         }
-
-        void generateRoutine(int month, int day)
+        public void generateAccounts()
         {
-            this.raceDay = day;
-            this.raceMonth = month; 
+            accounts.Add(new Account("user", "pass", "Chris"));
+            accounts.Add(new Account("user1", "pass", "Jessica"));
+            accounts.Add(new Account("user2", "pass", "Annie"));
+            accounts.Add(new Account("blank", "blank", "blank"));
         }
+        public void createRoutine(int accountNum)
+        {
+            if(accountNum == 0)
+            {
+                //Week 1
+                runs.Add(new Day("Dec", "Thu", 8, "Rest", nullInterval, 0, "0", 2));
+                runs.Add(new Day("Dec", "Fri", 9, "Run", nullInterval, 1.5, "5:30", 2));
+                runs.Add(new Day("Dec", "Sat", 10, "Rest", nullInterval, 0, "0", 2));
+                runs.Add(new Day("Dec", "Sun", 11, "Interval", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 1));
+                runs.Add(new Day("Dec", "Sun", 11, "Interval", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
+                runs.Add(new Day("Dec", "Sun", 11, "Interval", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
+                runs.Add(new Day("Dec", "Sun", 11, "Interval", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
 
+                //Week 2
+                runs.Add(new Day("Dec", "Thu", 8, "Rest1", nullInterval, 0, "0", 0));
+                runs.Add(new Day("Dec", "Fri", 9, "Run2", nullInterval, 1.5, "5:30", 0));
+                runs.Add(new Day("Dec", "Sat", 10, "Rest3", nullInterval, 0, "0", 0));
+                runs.Add(new Day("Dec", "Sun", 11, "Interval4", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
+                runs.Add(new Day("Dec", "Sun", 11, "Interval5", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
+                runs.Add(new Day("Dec", "Sun", 11, "Interval6", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
+                runs.Add(new Day("Dec", "Sun", 11, "Interval7", new Interval(2, 2.5, 3, 3.5), 2.0, "5:30", 0));
+
+            }
+            else if(accountNum == 1)
+            {
+                //Week 1
+                runs.Add(new Day("Dec", "Thu", 8, "Rest", nullInterval, 0, "0", 2));
+                runs.Add(new Day("Dec", "Fri", 9, "Run", nullInterval, 1.5, "5:30", 2));
+            }
+        }
         public struct Interval
         {
             public double interval1;
@@ -85,6 +97,66 @@ namespace RunOut.Data
             }
         }
 
+        public List<Account> accounts = new List<Account>();
+        public string currentUser = "user";
+        public string currentPassword = "pass";
+        public string currentName = "Chris";
+        public int accountNumber = 0;
 
+        public struct Account
+        {
+            public string user;
+            public string password;
+            public string name;
+
+            public string runName;
+            public int raceDay;
+            public int raceMonth;
+            public int raceYear;
+
+            public string raceDistance;
+            public string targetPace;
+
+            public Account(string user, string password, string name)
+            {
+
+                this.user = user;
+                this.password = password;
+                this.name = name;
+                runName = "Dash of Doom";
+                raceDay = 0;
+                raceMonth = 0;
+                raceYear = 2022;
+                raceDistance = "10";
+                targetPace = "6:40";
+
+            }
+			public Account(string user, string password, string name, string runName, int raceDay, int raceMonth, int raceYear, string raceDistance, string targetPace)
+			{
+
+				this.user = user;
+				this.password = password;
+				this.name = name;
+				this.runName = runName;
+				this.raceDay = raceDay;
+				this.raceMonth = raceMonth;
+				this.raceYear = raceYear;
+				this.raceDistance = raceDistance;
+				this.targetPace = targetPace;
+
+			}
+		}
+        public List<Feed> feed = new List<Feed>();
+        public struct Feed
+        {
+            public string name;
+            public string comment;
+            public string distance;
+            public string pace;
+
+            List<Feed> comments;
+
+
+        }
     }
 }
